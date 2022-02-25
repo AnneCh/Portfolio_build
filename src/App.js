5b8040fde53b87bbc9c6a4a9fb529ecd280baeea
Anne Chretien <chretien.an@gmail.com>
2022-02-25 15:20:49 +0100

    front page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1 className="h1">Hi, I'm Anne.</h1>
         <img src={tea} className="App-logo" alt="SomeTea?" />
         <p className='first-words'>I'm honored you're willing to know more about me.<br></br>
           Please, make yourself comfortable, and stay hydrated.
