5b43222261165e0a5d3279c8d9a4f2b799038c89
Anne Chretien <chretien.an@gmail.com>
2022-06-04 13:19:39 +0200

    boilerplate mod

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import Enter from './Enter.js';
-import './App.css';
 import * as React from 'react';
-import { Box } from '@mui/material';
+import { Box, makeStyles } from '@mui/material';
+import { BrowserRouter, Switch, Route, Link} from 'react-router-dom'
+import './App.css';
+import Enter from './Enter.js';
+
+const useStyles = makeStyles((theme) => {})
 
 function App() {
+  const classes = useStyles();
   return (
-    <Box component="div" sx={{ overflow: 'auto' }}>
-      <Enter />
-    </Box>
+    <BrowserRouter>    
+      <Box component="div" sx={{ overflow: 'auto' }}>
+        <Enter />
+      </Box>
+    </BrowserRouter> 
   );
 }
 
