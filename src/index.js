4dc1266882ab78337398202a4612fe92f86988e7
Anne Chretien <chretien.an@gmail.com>
2022-06-13 13:12:05 +0200

    logs

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +11,9 @@ render(<App />, rootElement);
 
  ReactDOM.render(
  <React.StrictMode>
-    <App />
+    <Router>
+     <App />
+    </Router>
 </React.StrictMode>,
     document.getElementById('root')
  );
