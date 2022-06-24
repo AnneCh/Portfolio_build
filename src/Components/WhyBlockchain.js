f07d77ce86ab69f3b8fed5ef41bf78c745d58804
Anne Chretien <chretien.an@gmail.com>
2022-06-24 15:25:04 +0200

    why Blockchain texts

--- src/Components/WhyBlockchain.js
+++ src/Components/WhyBlockchain.js
@@ -8,30 +8,17 @@ export default function WhyBlockchain() {
         <section id="about">
             <Container className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
                 <Container className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
-                 BlockChain Developer  </h1>
-                 <p>
-                    <br className="hidden lg:inline-block" />You may ask, "but why Blockchain?"
-                    Different experiences lead me to look for an alternative to the system I was brought up in. I was no longer convinced
-                    that what I what been used to would allow me to reach the goals that I had set for myself and my team.
-                    When I got introduced to the blockchain concept and Bitcoin, like most of us, I was dubitative. <br></br>
-                    
-                    Then, the more I gathered data, the more I caught myself thinking that, maybe, this could work.
-                    Maybe this system based on decentralization could lead to a more favorable ground to bridge the gaps.
-
-                    It looked like the best option from all the oens I had heard so far, giving 
-                    my limited financial means.
-
-                    I started taking it really seriously around October 2021, and the more I learn how to swiw, the deeper
-                    the pool gets. 
-                    It's exhilarating!
-
-                    I want to be part of a system that will aim at transforming the weaknesses of the previous systems into strengths,
-                    through transparency, immunatibility and decentralization. No single entity, wether person(s) or companies will ever 
-                    control markets and populations. 
-                    I want to be part of a system that allows actual people like me and my peers to decide which companies
-                    should receive state help to bring us closer all fulfiling our potential.
-                    </p>
+                <h1>{t('why_blockchain.title')}</h1>
+                <br></br>
+                <p>{t('why_blockchain.why')}</p>
+                <p>{t('why_blockchain.p1')}</p>
+                <p>{t('why_blockchain.p1bis')}</p>
+                <p>{t('why_blockchain.p2')}</p>
+                <p>{t('why_blockchain.p3')}</p>
+                <p>{t('why_blockchain.p4')}</p>
+                <p>{t('why_blockchain.p5')}</p>
+                <p>{t('why_blockchain.p6')}</p>
+                 
                 </Container>
             </Container>
         </section>
