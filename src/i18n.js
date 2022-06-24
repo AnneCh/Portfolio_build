3a9b4c658b8384eebe809f721f545e31cfdbced5
Anne Chretien <chretien.an@gmail.com>
2022-06-24 09:33:03 +0200

    i18n settings

--- src/i18n.js
+++ src/i18n.js
@@ -1,16 +1,21 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { LanguageDetectorModule } from 'i18next';
-import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
+import LanguageDetector from "i18next-browser-languagedetector"
 
 
 
 i18n
-    .use(LanguageDetectorModule)
-    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(initReactI18next) // passes i18n down to react-i18next
     .init({
         debug: true,
         fallbackLng: 'en', 
+        detection: {
+            order: ['htmlTag', 'cookie', 'localStorage', 'path', 'subdomain'],
+            caches: ['cookie'],
+            // those lines determine where i18n will go get the language preference from (order)
+            // and where to store the user's preference (caches)
+        },
         resources:{
             en:{
                 translation: {
