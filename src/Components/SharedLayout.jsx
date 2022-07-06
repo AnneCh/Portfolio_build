44e7bf2109743ca8e274f673fb562b450b9867f1
Anne Chretien <chretien.an@gmail.com>
2022-07-06 09:15:39 +0200

    update dependencies

--- src/Components/SharedLayout.jsx
+++ src/Components/SharedLayout.jsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router";
 import { NavBar } from "./Navbar";
-import { Alert, Container, ToggleButton } from '@mui/material';
+import { Container, ToggleButton } from '@mui/material';
 import i18n from "../i18n";
 
-//import darkTheme from '../theme';
-//import { ThemeProvider } from '@mui/material/styles';
+import darkTheme from '../Styles/Styles';
+import { ThemeProvider } from '@mui/material/styles';
 
 
 export default function SharedLayout() {
@@ -12,13 +12,13 @@ export default function SharedLayout() {
     i18n.changeLanguage(lng);
  }
     return(
-       //<ThemeProvider theme={darkTheme}>
+       <ThemeProvider theme={darkTheme}>
             <Container>    
-                <ToggleButton onClick={() => changeLanguage('en')} onMouseOver={Alert('Changer la langue?')}>en</ToggleButton>
-                <ToggleButton onClick={() => changeLanguage('fr')} onMouseOver={Alert('Change the language?')}>fr</ToggleButton>
+                <ToggleButton onClick={() => changeLanguage('en')}>en</ToggleButton>
+                <ToggleButton onClick={() => changeLanguage('fr')}>fr</ToggleButton>
                 <NavBar />
                 <Outlet />
             </Container>
-      //  </ThemeProvider>
+        </ThemeProvider>
     )
 }
