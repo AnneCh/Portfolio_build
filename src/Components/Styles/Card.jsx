695dbda6cef2de64def31851783654c0686b4782
Anne Chretien <chretien.an@gmail.com>
2022-07-31 18:49:50 +0200

    new cards

---
+++ src/Components/Styles/Card.jsx
@@ -0,0 +1,29 @@
+export const CardStyle = muiBaseTheme => ({
+    card: {
+      maxWidth: 200,
+      display: "flex",
+      margin: "auto",
+      transition: "0.3s",
+      boxShadow: "0 8px 40px -12px rgba(0,0,0,0.3)",
+      "&:hover": {
+        boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+      }
+    },
+    media: {
+      paddingTop: "56.25%"
+    },
+    content: {
+      textAlign: "left",
+      padding: muiBaseTheme.spacing.unit * 3
+    },
+    divider: {
+      margin: `${muiBaseTheme.spacing.unit * 3}px 0`
+    },
+    heading: {
+      fontWeight: "bold",
+      fontFamily: "Tangerine",
+    },
+    subheading: {
+      lineHeight: 1.8
+    },
+  });
