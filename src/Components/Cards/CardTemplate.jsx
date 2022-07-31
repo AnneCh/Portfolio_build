75850eae4e5cc6b21de47d0042bb55de8153caa9
Anne Chretien <chretien.an@gmail.com>
2022-07-31 17:56:58 +0200

    test cards

--- src/Components/Cards/CardTemplate.jsx
+++ src/Components/Cards/CardTemplate.jsx
@@ -1,3 +1,39 @@
 // a template for the cards that will be displayed on the WhatElse page 
 // displaying the other fields of interest that I have
 // and each will link to another component, a page, a video, whatever
+
+
+
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+export default function MediaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image="./Components/imgs/dark.png"
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          EXAMPLE TEST
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+}
--- src/Components/WhatElse.jsx
+++ src/Components/WhatElse.jsx
@@ -1,44 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useTranslation } from 'react-i18next'
-import { TerminalIcon, UsersIcon } from "@heroicons/react/solid";
 import { whatelse } from "./data.js";
 import { Container, Button } from '@mui/material';
+import MediaCard from './Cards/CardTemplate';
 
 function WhatElse() {
   const { t } = useTranslation()
 
-    return (
-      <Container>
-        <h1>  What else do I spend my time thinking about.</h1>
-            <div className="flex flex-wrap m-4">
-              {whatelse.map((whatelse) => (
-                <div className="p-4 md:w-1/2 w-full">
-                  <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-                    <p className="leading-relaxed mb-6">{whatelse.topic}</p>
-                    <div className="inline-flex items-center">
-                      <img
-                        alt=""
-                        src={whatelse.image}
-                        className="w-12 rounded-full flex-shrink-0 object-cover object-center"
-                      />
-                      <span className="flex-grow flex flex-col pl-4">
-                        <span className="title-font font-medium text-white">
-                          {whatelse.more}
-                        </span>
-                      </span>
-                    </div>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <Container>
-                <Link to="/">
-                    <Button>Home</Button>
-                </Link>
-            </Container>
-      </Container>
-      );
+     return (
+      <MediaCard/>
+
+
+
+
+
+    //   <Container>
+    //     <h1>  What else do I spend my time thinking about.</h1>
+    //         <div className="flex flex-wrap m-4">
+    //           {whatelse.map((whatelse) => (
+    //             <div className="p-4 md:w-1/2 w-full">
+    //               <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
+    //                 <p className="leading-relaxed mb-6">{whatelse.topic}</p>
+    //                 <div className="inline-flex items-center">
+    //                   <img
+    //                     alt=""
+    //                     src={whatelse.image}
+    //                     className="w-12 rounded-full flex-shrink-0 object-cover object-center"
+    //                   />
+    //                   <span className="flex-grow flex flex-col pl-4">
+    //                     <span className="title-font font-medium text-white">
+    //                       {whatelse.more}
+    //                     </span>
+    //                   </span>
+    //                 </div>
+    //               </div>
+    //             </div>
+    //           ))}
+    //         </div>
+    //         <Container>
+      //           <Link to="/">
+      //               <Button>Home</Button>
+      //           </Link>
+      //       </Container>
+      // </Container>
+       );
 }
 
 export default WhatElse
