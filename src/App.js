e3755c4bbdbf776c05df34ca52719fb50a67bea1
Anne Chretien <chretien.an@gmail.com>
2022-08-01 14:39:26 +0200

    added link

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from './Components/Enter';
-import MainDev from './Components/MainDev';
-import MainMul from './Components/MainMul';
-import Contact from './Components/Contact';
-import WhatElse from './Components/WhatElse';
-import Studies from './Components/Studies';
-import SharedLayout from './Components/SharedLayout';
-import Error from './Components/Error';
-import WhyBlockchain from './Components/WhyBlockchain';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import MainPage from './Components/Enter'
+import MainDev from './Components/MainDev'
+import MainMul from './Components/MainMul'
+import Contact from './Components/Contact'
+import WhatElse from './Components/WhatElse'
+import Studies from './Components/Studies'
+import SharedLayout from './Components/SharedLayout'
+import Error from './Components/Error'
+import WhyBlockchain from './Components/WhyBlockchain'
+import HealthInside from './Components/Cards/Insides/HealthCard_inside'
 
 export default function App() {
- 
-    return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<SharedLayout/>}>
-                        <Route index element={<MainPage/>} />
-                        <Route path="/MainDev" element={<MainDev />}/>
-                        <Route path="/MainMul" element={<MainMul />}/>
-                        <Route path="/Contact" element={<Contact />}/>
-                        <Route path="/WhatElse" element={<WhatElse />}/>
-                        <Route path="/Studies" element={<Studies />} />
-                        <Route path="/WhyBlockchain" element={<WhyBlockchain />} />
-                        <Route path="*" element={<Error />} />
-                        <Route path="/" element={<MainPage />}/>
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path="/MainDev" element={<MainDev />} />
+          <Route path="/MainMul" element={<MainMul />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/WhatElse" element={<WhatElse />} />
+          <Route path="/HealthCard_inside" element={<HealthInside />} />
+          <Route path="/Studies" element={<Studies />} />
+          <Route path="/WhyBlockchain" element={<WhyBlockchain />} />
+          <Route path="*" element={<Error />} />
+          <Route path="/" element={<MainPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
-
