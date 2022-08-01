cb83534c01ea7307667235255285b9b0f1d8e8e7
Anne Chretien <chretien.an@gmail.com>
2022-08-01 14:42:26 +0200

    added link

---
+++ src/Components/Cards/HealthCard.jsx
@@ -0,0 +1,40 @@
+// a template for the cards that will be displayed on the WhatElse page 
+// displaying the other fields of interest that I have
+// and each will link to another component, a page, a video, whatever
+
+
+
+import * as React from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, Divider} from '@mui/material';
+import health from './health.png'
+import { Link } from 'react-router-dom';
+
+import { CardStyle as classes} from '../Styles/Card';
+
+export default function Health() {
+  return (
+    <Card className={classes.card}>
+      <CardMedia className={classes.media}
+        component="img"
+        height="140"
+        width
+        src={health}
+        alt="/"
+      />
+      <CardContent className={classes.content}>
+        <Typography className={classes.heading} gutterBottom>
+          Health
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Research on endocrinology, foods, tactics to strengthen the immune system.
+        </Typography>
+      </CardContent>
+      <Divider className={classes.divider} light />
+      <CardActions>
+        <Link to="/HealthCard_inside">
+          <Button size="small">Learn More</Button>
+        </Link>
+      </CardActions>
+    </Card>
+  );
+}
---
+++ src/Components/Cards/Insides/HealthCard_inside.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import health from '../health.png';
+import { Paper } from "@mui/material";
+import { useTranslation } from "react-i18next";
+
+//create a Button in styles to go back to the previous page, or to go back to the Cards
+export default function HealthInside() {
+    const { t } = useTranslation()
+
+
+    return (
+        <Paper>
+           <img src={health}/>
+        </Paper>
+    )
+}
