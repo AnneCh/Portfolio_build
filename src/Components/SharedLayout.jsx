dff0ef5cbb774f9a7db6b39ac7120f74b7840114
Anne Chretien <chretien.an@gmail.com>
2022-08-02 17:56:05 +0200

    trying navbar

--- src/Components/SharedLayout.jsx
+++ src/Components/SharedLayout.jsx
@@ -1,21 +1,7 @@
 import * as React from 'react';
-import { useState, useMemo } from 'react';
-import i18n from "../i18n";
 import { Outlet } from "react-router";
-import { MaterialUISwitch as NightToggle} from "./Styles/Switch.jsx";
-
-import {
-    createTheme,
-    CssBaseline,
-    ThemeProvider,
-    Container, 
-    ToggleButton,
-    ToggleButtonGroup
-  } from "@mui/material"
-
-import { darkTheme } from './Styles/Dark.jsx';
-import { lightTheme } from './Styles/Light.jsx';
-import NavBar from "./Navbar";
+import {Container} from "@mui/material"
+import { NavBar } from "./Navbar";
 
 
 // ////!\\\\   work on styling the buttons 
@@ -24,43 +10,14 @@ import NavBar from "./Navbar";
 
 
 export default function SharedLayout() {
-    const changeLanguage = (lng) => {
-        i18n.changeLanguage(lng);
-    }
-
-    const [mode, setMode] = useState("light");
-    const toggleMode = () => {
-        // if the theme is not light, then set it to dark
-        if (mode === 'light') {
-            setMode('dark');
-        // otherwise, it should be light
-        } else {
-            setMode('light');
-        }
-      }
-
-    const theme = useMemo(
-      () => createTheme(mode === "light" ? lightTheme : darkTheme),
-      [mode]
-    );
+    
 
 
     return(
-        <ThemeProvider theme={theme}>
-        <CssBaseline enableColorScheme />
-        <ToggleButtonGroup orientation="vertical">
-            <Container  display='flex'>
-                <NightToggle onChange={toggleMode} align="center"/>
-            </Container>
-            <Container display='flex'>
-                <ToggleButton onClick={() => changeLanguage('en')}>en</ToggleButton>
-                <ToggleButton onClick={() => changeLanguage('fr')}>fr</ToggleButton>
-            </Container>
-            </ToggleButtonGroup>
+
             <Container>
                 <NavBar />
                 <Outlet />
             </Container>
-        </ThemeProvider>
     )
 }
