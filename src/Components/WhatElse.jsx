acfcdb312eec7de0d39eb39441a40d732d1b4f27
Anne Chretien <chretien.an@gmail.com>
2022-08-14 09:46:22 +0200

    clean

--- src/Components/WhatElse.jsx
+++ src/Components/WhatElse.jsx
@@ -8,62 +8,28 @@ const gridStyle = {
   marginTop: 5,
   marginLeft: "auto",
   marginRight: "auto",
-  maxWidth:"auto"
+  maxWidth:"auto",
+  
 }
 
 
 function WhatElse() {
 
      return (
-      <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 3}}>
       <Grid container sx={gridStyle} spacing={4}>
-          <Grid item xs={3} >
+          <Grid item >
             <Health/>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item >
             <MartialArts/>
           </Grid>
-          <Grid item xs={3}>
+          <Grid item >
             <MusicCard/>
           </Grid>
       </Grid>
     </Box>
-
-
-
-
-
-
-    //   <Container>
-    //     <h1>  What else do I spend my time thinking about.</h1>
-    //         <div className="flex flex-wrap m-4">
-    //           {whatelse.map((whatelse) => (
-    //             <div className="p-4 md:w-1/2 w-full">
-    //               <div className="h-full bg-gray-800 bg-opacity-40 p-8 rounded">
-    //                 <p className="leading-relaxed mb-6">{whatelse.topic}</p>
-    //                 <div className="inline-flex items-center">
-    //                   <img
-    //                     alt=""
-    //                     src={whatelse.image}
-    //                     className="w-12 rounded-full flex-shrink-0 object-cover object-center"
-    //                   />
-    //                   <span className="flex-grow flex flex-col pl-4">
-    //                     <span className="title-font font-medium text-white">
-    //                       {whatelse.more}
-    //                     </span>
-    //                   </span>
-    //                 </div>
-    //               </div>
-    //             </div>
-    //           ))}
-    //         </div>
-    //         <Container>
-      //           <Link to="/">
-      //               <Button>Home</Button>
-      //           </Link>
-      //       </Container>
-      // </Container>
-       );
+    );
 }
 
 export default WhatElse
