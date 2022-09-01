836b000d63e0626806663f7a8eb2c40c6c449e79
Anne Chretien <chretien.an@gmail.com>
2022-09-01 15:19:26 +0200

    added texts

--- src/Components/Cards/Insides/HealthCard_inside.jsx
+++ src/Components/Cards/Insides/HealthCard_inside.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import health from '../health.png';
-import { Paper } from "@mui/material";
+import { Container, Paper, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
 
 //create a Button in styles to go back to the previous page, or to go back to the Cards
@@ -9,6 +9,20 @@ export default function HealthInside() {
 
 
     return (
-           <img src={health}/>
+        <Container>
+           <img sx={{maxHeight:'50px', maxWdith:'50px'}} src={health}/>
+           <p>{t('health.h1')}</p>
+           <Typography>{t('health.description')}</Typography>
+           <Typography>{t('health.health1')}</Typography>
+           <Typography>{t('health.health2')}</Typography>
+           <Typography>{t('health.health3')}</Typography>
+           <Typography>{t('health.health4')}</Typography>
+           <Typography>{t('health.health5')}</Typography>
+           <Typography>{t('health.health6')}</Typography>
+           <Typography>{t('health.health7')}</Typography>
+           <Typography>{t('health.health8')}</Typography>
+           <Typography>{t('health.health9')}</Typography>
+
+        </Container>
     )
 }
--- src/Components/MainMul.jsx
+++ src/Components/MainMul.jsx
@@ -18,10 +18,10 @@ function MainMul() {
             <p>{t("main_mul.moving")}</p>
             <p>{t("main_mul.presence")}</p>
             <Container>
-            <h4>{t("main_mul.skills")}</h4>
-            <p>{t("main_mul.skills1")}</p>
-            <p>{t("main_mul.skills2")}</p>
-            <p>{t("main_mul.skills3")}</p>
+                <h4>{t("main_mul.skills")}</h4>
+                <p>{t("main_mul.skills1")}</p>
+                <p>{t("main_mul.skills2")}</p>
+                <p>{t("main_mul.skills3")}</p>
             </Container>
         </Container>
     )
