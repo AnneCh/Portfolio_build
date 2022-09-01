29e295765da4843803aaec532c8979852a083446
Anne Chretien <chretien.an@gmail.com>
2022-09-01 16:52:05 +0200

    change order

--- src/Components/Buttons.jsx
+++ src/Components/Buttons.jsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 
 export const HomeButton = () => {
     return(
-        <Button>
-            <Link to="/">
-                Ξ
-            </Link>
-        </Button>
+        <Link to="/">
+            <Button>
+                    Ξ
+            </Button>
+        </Link>
+
     )
 }
