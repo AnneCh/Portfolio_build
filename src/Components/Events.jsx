8e4074e55420516a6874ddf4a624c4fbcd136d79
Anne Chretien <chretien.an@gmail.com>
2023-07-24 19:31:58 +0200

    event page comment

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -3,6 +3,19 @@ import { Box } from "@mui/material";
 import React from "react";
 import { useTranslation } from 'react-i18next';
 
+
+/*
+MC
+DJ
+Rentals
+Sound Engineering
+Consulting
+Photography
+
+Have some kind of box for each with two lines maximum for each section, and a picture, similar to the main page 
+*/
+
+
 function Events() {
   const { t } = useTranslation()
 
