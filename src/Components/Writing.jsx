f119113514b5593d71f164832b7a429bfa9337ab
Anne Chretien <chretien.an@gmail.com>
2023-07-25 20:09:19 +0200

    link correction

--- src/Components/Writing.jsx
+++ src/Components/Writing.jsx
@@ -10,7 +10,7 @@ function Writing() {
       <Typography sx={{fontSize:"2rem"}}>{t('writing.pro')}</Typography><br/>
       <Typography sx={{fontSize:"4rem"}}>{t('writing.diverse')}</Typography><br/><br/>
       <Typography sx={{fontSize:"3rem"}}>{t('writing.mindset')}</Typography>
-      <Typography sx={{fontSize:"1.8rem"}}>Click <a target="blank" href="https://kevinbahler.notion.site/kevinbahler/Kevin-Bahler-1d52238340a74c80b57f442f97553559">here</a> to see some of the things we've written.</Typography>
+      <Typography sx={{fontSize:"1.8rem"}}>Click <a target="blank" href="https://kevinbahler.notion.site/Portfolio-03d694dc2708469581a9f77d5cb7dbd3">here</a> to see some of the things we've written.</Typography>
       <br/>
       <Typography sx={{fontSize:"2rem"}}>We also provide <a target="blank" href="https://drive.google.com/file/d/1Z7Wsi1LAbRqoTLW8QZA8-ryaHQw2qtKk/view?usp=sharing">redactions</a> and translations to and from French.</Typography>
     </Container>
