a4b6d6dc6ced13a3d6bcbfc01f580b1bc70328bd
Anne Chretien <chretien.an@gmail.com>
2023-07-26 19:57:55 +0200

    Others page

--- src/Components/Others.jsx
+++ src/Components/Others.jsx
@@ -1,14 +1,52 @@
-import { Box } from "@mui/material";
-import React from "react";
+import { Box, Button, Grid, Paper, Typography } from '@mui/material';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+const services = [
+  {
+    title: 'others.Consulting',
+    description: 'others.consulting',
+   },
+  {
+    title: 'others.Photo',
+    description: 'others.photo',
+    },
+  {
+    title: 'others.Websites',
+    description: 'others.website',
+  },
+  {
+    title: 'others.Autism',
+    description: 'others.autism',
+  },
+  {
+    title: 'others.MMA',
+    description: 'others.mma',
+     },
+  // Add more services...
+];
+
 function Others() {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
-    <Box sx={{textAlign:'center', margin:8,fontSize:30}}>
+    <Box sx={{ textAlign: 'center', margin: 'auto', marginTop: '4rem' }} maxWidth={false}>
+      <Grid container spacing={3} sx={{width:'100%'}}>
+        {services.map((service, index) => (
+          <Grid item xs={12} md={12} key={index}>
+            <Paper elevation={index % 2 === 0 ? 1 : 12} sx={{ padding: 3 }}>
+              <Typography variant="h5" gutterBottom>
+                {t(service.title)}
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                {t(service.description)}
+              </Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 }
 
-export default Others
+export default Others;
--- src/Components/Writing.jsx
+++ src/Components/Writing.jsx
@@ -6,13 +6,16 @@ function Writing() {
   const { t } = useTranslation()
 
   return (
-    <Container sx={{textAlign:'center', margin:'auto', marginTop:'4rem',}}>
+    <Container sx={{textAlign:'center', margin:'auto', marginTop:'4rem',}} maxWidth={false}>
       <Typography sx={{fontSize:"2rem"}}>{t('writing.pro')}</Typography><br/>
       <Typography sx={{fontSize:"4rem"}}>{t('writing.diverse')}</Typography><br/><br/>
       <Typography sx={{fontSize:"3rem"}}>{t('writing.mindset')}</Typography>
-      <Typography sx={{fontSize:"1.8rem"}}>Click <a target="blank" href="https://kevinbahler.notion.site/Portfolio-03d694dc2708469581a9f77d5cb7dbd3">here</a> to see some of the things we've written.</Typography>
       <br/>
       <Typography sx={{fontSize:"2rem"}}>We also provide <a target="blank" href="https://drive.google.com/file/d/1Z7Wsi1LAbRqoTLW8QZA8-ryaHQw2qtKk/view?usp=sharing">redactions</a> and translations to and from French.</Typography>
+      <br/>
+      <Typography sx={{ fontSize: "3rem", textShadow: '2px 2px 4px rgba(10, 10, 10, 0.5)' }}>
+        Click <a target="_blank" href="https://kevinbahler.notion.site/Portfolio-03d694dc2708469581a9f77d5cb7dbd3">here</a> to see some of the things we've written.
+      </Typography>
     </Container>
   );
 }
