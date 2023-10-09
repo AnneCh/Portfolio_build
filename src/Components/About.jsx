af77d472576ebee8937853db35a84d3db6c57fc9
Anne Chretien <chretien.an@gmail.com>
2023-10-09 19:07:41 +0200

    Change in about and main page sections

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,20 +1,125 @@
-import { Box, Container } from "@mui/material";
-import React from "react";
-import { useTranslation } from 'react-i18next';
-import BackButton from "./BackButton";
 
-function About() {
+
+import {Container, Typography, Box } from '@mui/material';
+import * as React from 'react';
+import { useTranslation } from 'react-i18next';
+import Avatar from '@mui/material/Avatar';
+import jim from './jim.png';
+import kevin from './kevin.png';
+import anne from './anne.png';
+import { Link } from 'react-router-dom';
+
+import IconButton from '@mui/material/IconButton';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+
+const SocialMediaIcons = ({ linkedin, twitter, youtube }) => (
+  <Box 
+    sx={{ 
+      display: 'flex', 
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 2
+    }}
+  >
+    <IconButton href={linkedin} target="_blank" aria-label="LinkedIn">
+      <LinkedInIcon />
+    </IconButton>
+    <IconButton href={twitter} target="_blank" aria-label="Twitter">
+      <TwitterIcon />
+    </IconButton>
+    <IconButton href={youtube} target="_blank" aria-label="YouTube">
+      <YouTubeIcon />
+    </IconButton>
+  </Box>
+);
+
+
+
+const ImageTextComponent = ({ imageUrl, text, linkedin, twitter, youtube }) => (
+<Box 
+    sx={{ 
+      display: 'flex', 
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: 2
+    }}
+  >
+    <Box 
+      sx={{ 
+        display: 'flex', 
+        alignItems: 'center',
+        gap: 10
+      }}
+    >
+      <Avatar 
+        src={imageUrl} 
+        alt="image description"
+        sx={{ 
+          width: 350,
+          height: 350
+        }}
+      />
+      <Typography variant="body1" fontSize={'150%'}>{text}</Typography>
+    </Box>
+    <SocialMediaIcons linkedin={linkedin} twitter={twitter} youtube={youtube} />
+  </Box>
+);
+
+
+const ImageTextRevert = ({ imageUrl, text, linkedin, twitter, youtube }) => (
+<Box 
+    sx={{ 
+      display: 'flex', 
+      flexDirection: 'column',
+      alignItems: 'center',
+      gap: 10
+    }}
+  >
+    <Box 
+      sx={{ 
+        display: 'flex', 
+        alignItems: 'center',
+        gap: 10
+      }}
+    >
+      <Typography variant="body1" fontSize={'150%'}>{text}</Typography>
+      <Avatar 
+        src={imageUrl} 
+        alt="image description"
+        sx={{ 
+          width: 350,
+          height: 350
+        }}
+      />
+    </Box>
+    <SocialMediaIcons linkedin={linkedin} twitter={twitter} youtube={youtube} />
+  </Box>
+);
+
+
+export default function About() {
   const { t } = useTranslation()
 
   return (
-    <Container>
-      <Box>
-        <BackButton/>
-      </Box>
-      <Box sx={{textAlign:'center', margin:8,fontSize:30}}>
-      </Box>
-    </Container>
+    <>
+      <Container sx={{ paddingTop:"40px"}}>
+        <Typography sx={{fontSize:"80px", fontWeight:"bold", textAlign:'center'}}>{t('main_page.visitor')}</Typography>
+        <br/>
+        <Typography sx={{fontSize:"45px", textAlign:'center'}}>{t('main_page.hydrate')}</Typography>
+        <Typography sx={{fontSize:"20px", textAlign:'center'}}>{t('main_page.weird')}</Typography>
+        <Typography sx={{fontSize:"20px", fontStyle:'italic', textAlign:'center'}}>{t('main_page.think')}</Typography>
+        <Link to='/Jim' style={{ textDecoration: 'none' }}><ImageTextComponent imageUrl={jim} text={t('main_page.jim')} linkedin="" youtube="" twitter=""/></Link>
+        <a href='https://kevinbahler.notion.site/CV-aed239f939e34d25bb628b7b14cee2e7' target="_blank" style={{ textDecoration: 'none' }} linkedin="" youtube="" twitter="">  <ImageTextRevert imageUrl={kevin} text={t('main_page.kevin')}/></a>
+        <Link to='/Anne' style={{ textDecoration: 'none' }}><ImageTextComponent imageUrl={anne} text={t('main_page.anne')} linkedin="" youtube="" twitter="https://twitter.com/AnneMonChi"/></Link>
+      </Container>
+      <Container>
+
+      </Container>
+      </>
   );
 }
 
-export default About
+
--- src/Components/Enter.jsx
+++ src/Components/Enter.jsx
@@ -1,123 +1,75 @@
-import {Container, Typography, Box } from '@mui/material';
-import * as React from 'react';
+import { Box, Grid, Typography, Button } from '@mui/material';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
-import Avatar from '@mui/material/Avatar';
-import jim from './jim.png';
-import kevin from './kevin.png';
-import anne from './anne.png';
-import { Link } from 'react-router-dom';
-
-import IconButton from '@mui/material/IconButton';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import YouTubeIcon from '@mui/icons-material/YouTube';
+import BackButton from "./BackButton";
 
 
-const SocialMediaIcons = ({ linkedin, twitter, youtube }) => (
-  <Box 
-    sx={{ 
-      display: 'flex', 
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap: 2
-    }}
-  >
-    <IconButton href={linkedin} target="_blank" aria-label="LinkedIn">
-      <LinkedInIcon />
-    </IconButton>
-    <IconButton href={twitter} target="_blank" aria-label="Twitter">
-      <TwitterIcon />
-    </IconButton>
-    <IconButton href={youtube} target="_blank" aria-label="YouTube">
-      <YouTubeIcon />
-    </IconButton>
-  </Box>
-);
+const services = [
+  {
+    title: 'main.Copywriting',
+    description: 'main.copywriting',
+    image:'/music10.jpg',
+  },
+  {
+    title: 'main.Equipment',
+    description: 'main.equipment',
+    image:'/music12.jpg',
+  },
+  {
+    title: 'main.Edition',
+    description: 'main.edition',
+    image:'/music23.jpg',
+  },
+  {
+    title: 'main.More',
+    description: 'main.more',
+    image:'/music16.jpg',
+  },
+];
 
-
-
-const ImageTextComponent = ({ imageUrl, text, linkedin, twitter, youtube }) => (
-<Box 
-    sx={{ 
-      display: 'flex', 
-      flexDirection: 'column',
-      alignItems: 'center',
-      gap: 2
-    }}
-  >
-    <Box 
-      sx={{ 
-        display: 'flex', 
-        alignItems: 'center',
-        gap: 10
-      }}
-    >
-      <Avatar 
-        src={imageUrl} 
-        alt="image description"
-        sx={{ 
-          width: 350,
-          height: 350
-        }}
-      />
-      <Typography variant="body1" fontSize={'150%'}>{text}</Typography>
-    </Box>
-    <SocialMediaIcons linkedin={linkedin} twitter={twitter} youtube={youtube} />
-  </Box>
-);
-
-
-const ImageTextRevert = ({ imageUrl, text, linkedin, twitter, youtube }) => (
-<Box 
-    sx={{ 
-      display: 'flex', 
-      flexDirection: 'column',
-      alignItems: 'center',
-      gap: 10
-    }}
-  >
-    <Box 
-      sx={{ 
-        display: 'flex', 
-        alignItems: 'center',
-        gap: 10
-      }}
-    >
-      <Typography variant="body1" fontSize={'150%'}>{text}</Typography>
-      <Avatar 
-        src={imageUrl} 
-        alt="image description"
-        sx={{ 
-          width: 350,
-          height: 350
-        }}
-      />
-    </Box>
-    <SocialMediaIcons linkedin={linkedin} twitter={twitter} youtube={youtube} />
-  </Box>
-);
-
-
-export default function MainPage() {
-  const { t } = useTranslation()
-
+function MainPage() {
+  const { t } = useTranslation();
   return (
-    <>
-      <Container sx={{ paddingTop:"40px"}}>
-        <Typography sx={{fontSize:"80px", fontWeight:"bold", textAlign:'center'}}>{t('main_page.visitor')}</Typography>
-        <br/>
-        <Typography sx={{fontSize:"45px", textAlign:'center'}}>{t('main_page.hydrate')}</Typography>
-        <Typography sx={{fontSize:"20px", textAlign:'center'}}>{t('main_page.weird')}</Typography>
-        <Typography sx={{fontSize:"20px", fontStyle:'italic', textAlign:'center'}}>{t('main_page.think')}</Typography>
-        <Link to='/Jim' style={{ textDecoration: 'none' }}><ImageTextComponent imageUrl={jim} text={t('main_page.jim')} linkedin="" youtube="" twitter=""/></Link>
-        <a href='https://kevinbahler.notion.site/CV-aed239f939e34d25bb628b7b14cee2e7' target="_blank" style={{ textDecoration: 'none' }} linkedin="" youtube="" twitter="">  <ImageTextRevert imageUrl={kevin} text={t('main_page.kevin')}/></a>
-        <Link to='/Anne' style={{ textDecoration: 'none' }}><ImageTextComponent imageUrl={anne} text={t('main_page.anne')} linkedin="" youtube="" twitter="https://twitter.com/AnneMonChi"/></Link>
-      </Container>
-      <Container>
-
-      </Container>
-      </>
+    <Box sx={{ textAlign: 'center', margin: 'auto', marginTop: '4rem' }}>
+      <BackButton/>
+      <Grid container spacing={3}>
+        {services.map((service, index) => (
+          <Grid item xs={12} sx={{ width: '100%' }} key={index}>
+            <Box 
+              sx={{ 
+                padding: 3,
+                position: 'relative', // Relative positioning
+                overflow: 'hidden', // So that the pseudo-element doesn't overflow
+                '&::after': {
+                  content: '""', // necessary for the pseudo-element
+                  display: 'block',
+                  position: 'absolute',
+                  top: 0,
+                  right: 0,
+                  bottom: 0,
+                  left: 0,
+                  backgroundImage: `url(${service.image})`,
+                  backgroundSize: 'cover',
+                  opacity: 0.20, // 20% opacity for the image
+                  zIndex: -1, // Placed behind the content
+                }
+              }}
+            >
+              <Typography variant="h5" gutterBottom>
+                {t(service.title)}
+              </Typography>
+              <Typography variant="body1" gutterBottom>
+                {t(service.description)}
+              </Typography>
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+      <Button sx={{marginTop:"1.2rem"}}variant="contained" color="primary" href="/contact">
+        {t('others.contact')}
+      </Button>
+    </Box>
   );
 }
 
-
+export default MainPage;
